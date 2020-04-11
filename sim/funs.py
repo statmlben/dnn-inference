a0067a7adbf4ad2f5eac79cf928a9691149377a1
statmlben <bendaimath@gmail.com>
2020-04-11 00:22:22 -0500

    all

--- sim/funs.py
+++ sim/funs.py
@@ -97,17 +97,6 @@ class DeepT(object):
 
 	def reset_model(self):
 		if int(tf.__version__[0]) == 2:
-			# for layer in self.model.layers: 
-			# 	if isinstance(layer, tf.keras.Model):
-			# 		reset_weights(layer)
-			# 		continue
-			# 	for k, initializer in layer.__dict__.items():
-			# 		if "initializer" not in k:
-			# 			continue
-			# 			# find the corresponding variable
-			# 		var = getattr(layer, k.replace("_initializer", ""))
-			# 		var.assign(initializer(var.shape, var.dtype))
-
 			for layer in self.model.layers:
 				if isinstance(layer, tf.keras.Model): #if you're using a model as a layer
 					reset_weights(layer) #apply function recursively
@@ -347,55 +336,6 @@ class DeepT(object):
 				n_opt = len(X) - 2*m_opt
 			return n_opt, m_opt
 
-
-	# def adaRatio(self, X, y, k=0, fit_params={}, num_perm=100, ratio_grid=[.1, .2, .3, .4], min_inf=50, verbose=0):
-	# 	found = 0
-	# 	for ratio_tmp in reversed(ratio_grid):
-	# 		self.reset_model()
-	# 		m_tmp = int(len(X)*ratio_tmp)
-	# 		n_tmp = len(X) - 2*m_tmp
-	# 		# split data
-	# 		X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=n_tmp, random_state=42)
-	# 		# permutate training sample
-	# 		y_train_perm = np.random.permutation(y_train)
-	# 		# training for full model
-	# 		history = self.model.fit(x=X_train, y=y_train_perm, **fit_params)
-	# 		# training for mask model
-	# 		Z_train = self.mask_cov(X_train, k)
-	# 		history_mask = self.model_mask.fit(x=Z_train, y=y_train_perm, **fit_params)
-	# 		## evaluate the performance
-	# 		P_value = []
-	# 		for t in range(num_perm):
-	# 			# permutate testing sample
-	# 			y_test_perm = np.random.permutation(y_test)
-	# 			# split two sample
-	# 			X_inf, X_inf_mask, y_inf, y_inf_mask = train_test_split(X_test, y_test_perm, train_size=m_tmp, random_state=42)
-	# 			Z_inf = self.mask_cov(X_inf_mask, k)
-	# 			# evaluation
-	# 			pred_y = self.model.predict(X_inf).flatten()
-	# 			pred_y_mask = self.model_mask.predict(Z_inf).flatten()
-	# 			SE_tmp = (pred_y - y_inf)**2
-	# 			SE_mask_tmp = (pred_y_mask - y_inf_mask)**2
-	# 			Lambda_tmp = np.sqrt(m_tmp) * ( SE_tmp.std()**2 + SE_mask_tmp.std()**2 )**(-1/2)*( SE_tmp.mean() - SE_mask_tmp.mean() )
-	# 			p_value_tmp = norm.cdf(Lambda_tmp)
-	# 			P_value.append(p_value_tmp)
-	# 		P_value = np.array(P_value)
-	# 		## compute the type 1 error
-	# 		Err1 = len(P_value[P_value<self.alpha])/len(P_value)
-	# 		if verbose==1:
-	# 			print('Type 1 error: %.3f; inference sample ratio: %.3f' %(Err1, ratio_tmp))
-	# 		if Err1 < self.alpha:
-	# 			found = 1
-	# 			break
-	# 	if found==0:
-	# 		warnings.warn("No ratio can control the Type 1 error, pls increase the sample size, and inference sample ratio is set as the min of ratio_grid.")
-	# 	if m_tmp < min_inf:
-	# 		warnings.warn("The estimated inference sample is too small, pls increase the sample size, and inference sample is set as 100")
-	# 		m_tmp = min_inf
-	# 		n_tmp = len(X) - 2*m_tmp
-	# 	return n_tmp, m_tmp
-
-
 	def testing(self, X, y, pred_size=None, inf_size=None, fit_params={}, split_params={}):
 		P_value = []
 		for k in range(len(self.inf_cov)):
@@ -432,3 +372,108 @@ class DeepT(object):
 		return P_value, SE.mean()
 
 
+class PermT(object):
+	def __init__(self, inf_cov, model, model_mask, alpha=.05, verbose=0):
+		self.inf_cov = inf_cov
+		self.model = model
+		self.model_perm = model_perm
+		self.alpha = alpha
+		self.num_perm = 100
+
+	def reset_model(self):
+		if int(tf.__version__[0]) == 2:
+			# for layer in self.model.layers: 
+			# 	if isinstance(layer, tf.keras.Model):
+			# 		reset_weights(layer)
+			# 		continue
+			# 	for k, initializer in layer.__dict__.items():
+			# 		if "initializer" not in k:
+			# 			continue
+			# 			# find the corresponding variable
+			# 		var = getattr(layer, k.replace("_initializer", ""))
+			# 		var.assign(initializer(var.shape, var.dtype))
+
+			for layer in self.model.layers:
+				if isinstance(layer, tf.keras.Model): #if you're using a model as a layer
+					reset_weights(layer) #apply function recursively
+					continue
+
+				#where are the initializers?
+				if hasattr(layer, 'cell'):
+					init_container = layer.cell
+				else:
+					init_container = layer
+
+				for key, initializer in init_container.__dict__.items():
+					if "initializer" not in key: #is this item an initializer?
+					  continue #if no, skip it
+
+					# find the corresponding variable, like the kernel or the bias
+					if key == 'recurrent_initializer': #special case check
+						var = getattr(init_container, 'recurrent_kernel')
+					else:
+						var = getattr(init_container, key.replace("_initializer", ""))
+					
+					if var is None:
+						continue
+					else:
+						var.assign(initializer(var.shape, var.dtype))
+		
+		if int(tf.__version__[0]) == 1:
+			session = K.get_session()
+			for layer in self.model.layers:
+				if hasattr(layer, 'kernel_initializer'):
+					layer.kernel.initializer.run(session=session)
+			for layer in self.model_mask.layers:
+				if hasattr(layer, 'kernel_initializer'):
+					layer.kernel.initializer.run(session=session)
+
+
+	## can be extent to @abstractmethod
+	def mask_cov(self, X, k=0, type_='vector'):
+		if type_ == 'vector':
+			Z = X.copy()
+			Z[:,self.inf_cov[k]]= 0.
+		return Z
+
+	def perm_cov(self, X, k=0, type_='vector'):
+		if type_ == 'vector':
+			Z = X.copy()
+			Z[:,self.inf_cov[k]]= np.random.permutation(Z[:,self.inf_cov[k]])
+		return Z
+
+	def noise_cov(self, X, k=0, type_='vector'):
+		if type_ == 'vector':
+			Z = X.copy()
+			Z[:,self.inf_cov[k]] = np.random.randn(len(X), len(self.inf_cov[k]))
+		return Z
+
+	def testing(self, X, y, fit_params={}):
+		P_value = []
+		for k in range(len(self.inf_cov)):
+			self.reset_model()
+			print('%d-th inference; permutation testing' %k)
+			## prediction and inference in full model
+			history = self.model.fit(X, y, **fit_params)
+			pred_y = self.model.predict(X).flatten()
+			SE = (pred_y - y)**2
+			score = SE.copy()
+			# prediction and inference in mask model
+			score_perm = []
+			for l in range(self.num_perm):
+				Z = self.perm_cov(X, k)
+				history_perm = self.model_perm.fit(Z, y, **fit_params)
+				pred_y_perm = self.model_perm.predict(Z).flatten()
+				SE_perm = (pred_y_perm - y)**2
+				score_perm.append(SE_perm)
+			score_perm = np.array(score_perm)
+			## compute p-value
+			p_value_tmp = (np.sum(score_perm >= score) + 1.0) / (self.num_perm + 1)
+
+			if p_value_tmp < self.alpha:
+				print('reject H0 with p_value: %.3f' %p_value_tmp)
+			else:
+				print('accept H0 with p_value: %.3f' %p_value_tmp)
+
+			P_value.append(p_value_tmp)
+		return P_value, SE.mean()
