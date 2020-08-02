ec4672ba702987494a9a6cc113fbf05e06357c6c
statmlben <bendaimath@gmail.com>
2020-08-01 23:42:17 -0500

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -29,20 +29,20 @@ sys.path.insert(0, os.path.abspath('..'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-# extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax', 'sphinx.ext.napoleon', 'nbsphinx',
-#                 'nbsphinx_link', 'recommonmark', 'sphinx_git']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax', 'sphinx.ext.napoleon', 'nbsphinx',
+                'nbsphinx_link', 'recommonmark', 'sphinx_git']
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.ifconfig',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode'
-]
+# extensions = [
+#     'sphinx.ext.autodoc',
+#     'sphinx.ext.doctest',
+#     'sphinx.ext.intersphinx',
+#     'sphinx.ext.todo',
+#     'sphinx.ext.coverage',
+#     'sphinx.ext.mathjax',
+#     'sphinx.ext.ifconfig',
+#     'sphinx.ext.napoleon',
+#     'sphinx.ext.viewcode'
+# ]
 
 
 # Add any paths that contain templates here, relative to this directory.
