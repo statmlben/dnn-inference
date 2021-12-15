6bd8b0826ee0688f4f6a0a2f046399019e13f50b
statmlben <bendaimath@gmail.com>
2021-12-15 10:35:12 +0800

    dec15|add master doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,6 +36,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+master_doc = 'index'
 extensions = [
 	'sphinx.ext.autodoc',
     # "sphinx.ext.linkcode",
