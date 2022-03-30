f17b6945dfff80118e25f1b39b56c3e4027b3b92
Ben Dai <33288866+statmlben@users.noreply.github.com>
2022-03-30 20:22:10 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages, Extension
+
+with open('README.rst') as f:
+    LONG_DESCRIPTION = f.read()
+
+if __name__ == "__main__":
+    setup(
+        # Needed to silence warnings (and to be a worthwhile package)
+        name='dnn-inference',
+        url='https://github.com/statmlben/dnn-inference',
+        author='Ben Dai',
+        author_email='bendai@cuhk.edu.hk',
+        # Needed to actually package something
+        packages=['dnn_inference'],
+        # Needed for dependencies
+        install_requires=['numpy', 'keras', 'tensorflow>=2.3.1', 'scipy', 'sklearn', 'matplotlib'],
+        # *strongly* suggested for sharing
+        version='0.14',
+        # The license can be anything you like
+        license='MIT',
+        description='Dnn-Inference is a Python module for hypothesis testing based on deep neural networks.',
+        #cmdclass={"build_ext": build_ext},
+        # We will also need a readme eventually (there will be a warning)
+        long_description_content_type='text/x-rst',
+        long_description=LONG_DESCRIPTION,
+    )
